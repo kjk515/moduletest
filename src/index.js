441119980bac7baf1a8ec20f7da47023f5869123
강진경 <jkkang@wemakeprice.com>
2020-08-12 16:33:48 +0900

    lodash test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-//import { funcCm1 } from './util/util-commonjs';
+import { cloneDeep } from 'lodash-es' // 번들링 포함되지 않음
+import intersectionWith from 'lodash/intersectionWith' // 번들링 포함됨
 
-//funcCm1();
+import { funcCm1 } from './util/util-commonjs'; // 번들링 포함됨
+import { funcEsm1 } from './util/util-esm'; // 번들링 포함되지 않음
 
 export function libFunc() {
   console.log('indexFunc');
