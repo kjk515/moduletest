60b92924123f34332a90b56eacc9faaa2b32b776
강진경 <jkkang@wemakeprice.com>
2020-08-13 12:08:35 +0900

    module test

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path')
 
 module.exports = {
+  //entry: {
+  //  util1: './src/util/util-commonjs.js',
+  //  util2: './src/util/util-esm.js',
+  //},
+  //output: {
+  //  filename: '[name].js',
   entry: './src/index.js',
   output: {
     filename: 'lib.js',
